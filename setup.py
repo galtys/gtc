41417e5dfb2771a4785a6cc3ebfe749a6316b41d
Jan <accounts@galtys.com>
2014-08-08 14:09:36 +0100

    added setup

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='gtc',
+    version='1.1',
+    description='experimental module to manage odoo deployments',
+    author='Jan Troler',
+    author_email='jan.troler@galtys.com',
+    url='git@codebasehq.com:galtys/galtys/gtc.git',
+    packages=['gtc'],
+    install_requires=[
+        #'requests>=2.0.1',
+    ],
+    license='MIT',
+    scripts=[
+        'bin/gtc'
+    ],
+)
